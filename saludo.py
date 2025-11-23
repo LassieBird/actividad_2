dca313086a822ff3c938b6fa40200938a8025126
ruth_mdz <mendezbustos.ruthesmeralda.2021@gmail.com>
2025-11-23 01:45:49 -0600

    Se ha creado el archivo saludo.py en la rama rama_uno

---
+++ saludo.py
@@ -0,0 +1,4 @@
+print("¡Hola, este es un archivo .py que saluda al usuario!")
+nombre = input("\n ¿Cual es tu nombre? ")
+print(f"\n Hola {nombre.upper()}!")
+print("Espero que tengas un bonito día!")
